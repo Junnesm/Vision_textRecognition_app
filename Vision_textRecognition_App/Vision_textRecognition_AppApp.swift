07adeb586e0a77bf345b23b9b9ceafe85941a46a
Junnesm <JUNNE.MURDOCK7645@stu.mtec.eduec.edu>
2023-06-07 18:58:24 -0600

    staging

---
+++ Vision_textRecognition_App/Vision_textRecognition_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_textRecognition_AppApp.swift
+//  Vision_textRecognition_App
+//
+//  Created by Junne Murdock on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Vision_textRecognition_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vision_textRecognition_AppUITests/Vision_textRecognition_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Vision_textRecognition_AppUITestsLaunchTests.swift
+//  Vision_textRecognition_AppUITests
+//
+//  Created by Junne Murdock on 6/7/23.
+//
+
+import XCTest
+
+final class Vision_textRecognition_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
