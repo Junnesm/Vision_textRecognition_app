19029cdf08989c5616b251abf28778904f083ef8
Junnesm <JUNNE.MURDOCK7645@stu.mtec.eduec.edu>
2023-06-07 19:03:57 -0600

    renamed content view

--- Vision_textRecognition_App/Vision_textRecognition_AppApp.swift
+++ Vision_textRecognition_App/Vision_textRecognition_AppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Vision_textRecognition_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainVisionView()
         }
     }
 }
